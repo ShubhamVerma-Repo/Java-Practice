0c626fec5189ce0e99a5e8113b22495ed3a95f74
shubham.verma <shubham.verma@IRIS-CSG-1630.irissoftware.com>
2019-08-23 18:49:12 +0530

    Java Reflection Practice

---
+++ JavaReflection/src/com/iris/javaReflection/DemoClass.java
@@ -0,0 +1,16 @@
+package com.iris.javaReflection;
+
+public class DemoClass {
+
+	private String shubham;
+	private String verma;
+	
+	public String getDailyWorkout() {
+		return "hey, get a daily workout.";
+	}
+
+	
+	public String getDailyFortune() {
+		return "hey, have a daily fortune.";
+	}	
+}
---
+++ JavaReflection/src/com/iris/javaReflection/MainApp.java
@@ -0,0 +1,32 @@
+package com.iris.javaReflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		Method[] methods=DemoClass.class.getMethods();
+		
+		System.out.println("here is the list of democlass methods : \n");
+		int i=1;
+		/*for(Method getMethod:methods)
+		{
+			System.out.println("method "+i+" :  "+getMethod.getName());
+			i++;
+		}*/
+		
+		
+		Class myObjectClass = DemoClass.class;
+
+		Field[] fields=myObjectClass.getFields();
+		
+		for(Field getField:fields)
+		{
+			System.out.println("fields "+i+" :  "+getField.getName());
+			i++;
+		}
+	}
+
+}
